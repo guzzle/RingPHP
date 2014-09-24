7d39cc079da1216b1aa0ec75deaaffc27328df43
Michael Dowling <mtdowling@gmail.com>
2014-09-23 21:18:16 -0700

    Cleaning up future value

--- src/FutureValue.php
+++ src/FutureValue.php
@@ -1,15 +1,29 @@
 <?php
 namespace GuzzleHttp\Ring;
 
+use GuzzleHttp\Ring\Exception\CancelledFutureAccessException;
+
 /**
  * Represents a simple future value that responds to deref to retrieve a value.
  */
 class FutureValue implements FutureInterface
 {
-    use MagicFutureTrait;
+    use BaseFutureTrait;
 
-    protected function processResult($result)
+    private $cache;
+
+    public function deref()
     {
-        return $result;
+        if (!isset($this->cache)) {
+            if ($this->isCancelled) {
+                throw new CancelledFutureAccessException('You are attempting '
+                    . 'to access a future that has been cancelled.');
+            }
+            $deref = $this->dereffn;
+            $this->dereffn = $this->cancelfn = null;
+            $this->cache = $deref();
+        }
+
+        return $this->cache;
     }
 }
--- tests/FutureValueTest.php
+++ tests/FutureValueTest.php
@@ -7,9 +7,27 @@ class FutureValueTest extends \PHPUnit_Framework_TestCase
 {
     public function testDerefReturnsValue()
     {
-        $f = new FutureValue(function () {
+        $called = 0;
+        $f = new FutureValue(function () use (&$called) {
+            $called++;
             return 'foo';
         });
         $this->assertEquals('foo', $f->deref());
+        $this->assertEquals(1, $called);
+        $this->assertEquals('foo', $f->deref());
+        $this->assertEquals(1, $called);
+        $this->assertFalse($f->cancelled());
+        $this->assertFalse($f->cancel());
+        $this->assertTrue($f->realized());
+    }
+
+    /**
+     * @expectedException \GuzzleHttp\Ring\Exception\CancelledFutureAccessException
+     */
+    public function testThrowsWhenAccessingCancelled()
+    {
+        $f = new FutureValue(function () {}, function () { return true; });
+        $this->assertTrue($f->cancel());
+        $f->deref();
     }
 }
