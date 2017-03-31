6218c409739ab2db2a2c9d30d56925fd1936dff6
Remi Collet <fedora@famillecollet.com>
2017-03-31 08:29:47 +0200

    Fix #41 use proper proxy attribute without ending /

--- tests/Client/StreamHandlerTest.php
+++ tests/Client/StreamHandlerTest.php
@@ -375,7 +375,7 @@ class StreamHandlerTest extends \PHPUnit_Framework_TestCase
 
     public function testAddsProxyByProtocol()
     {
-        $url = str_replace('http', 'tcp', Server::$url);
+        $url = rtrim(str_replace('http', 'tcp', Server::$url), '/');
         $res = $this->getSendResult(['proxy' => ['http' => $url]]);
         $opts = stream_context_get_options($res['body']);
         $this->assertEquals($url, $opts['http']['proxy']);
